3d8de1df1c42fa812dbb9a516fda258bfd5d221f
KraosFex <ferreirajohan14253@gmail.com>
2022-06-01 09:24:14 -0400

    favicon update

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-// Environment variable
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
-
 // import components
 import App from './components/smartComponents/app/App';
+
+// environment variable
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 
 ReactDOM.render( <App />, document.getElementById('root'));
